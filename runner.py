66b564b5b8d4e86f1b87925fb26818c751e07b64
realhardik18 <realhardik18@gmail.com>
2022-09-05 15:38:30 +0530

    got it to upload video programatically

--- runner.py
+++ runner.py
@@ -1,15 +1,41 @@
-import os
+import datetime
 from Google import Create_Service
-import pandas as pd
+from googleapiclient.http import MediaFileUpload
 
-API_NAME = 'youtube'
-API_VERSION = 'v1'
 CLIENT_SECRET_FILE = 'creds.json'
-SCOPES = [
-    'https://www.googleapis.com/auth/youtube.upload'
-
-]
+API_NAME = 'youtube'
+API_VERSION = 'v3'
+SCOPES = ['https://www.googleapis.com/auth/youtube.upload']
 
 service = Create_Service(CLIENT_SECRET_FILE, API_NAME, API_VERSION, SCOPES)
-print(service.videos())
-# work from here
+
+upload_date_time = datetime.datetime(
+    2020, 12, 25, 12, 30, 0).isoformat() + '.000Z'
+
+request_body = {
+    'snippet': {
+        'categoryId': 19,
+        'title': 'Upload Testing',
+        'description': 'Hello World Description',
+        'tags': ['Travel', 'video test', 'Travel Tips']
+    },
+    'status': {
+        'privacyStatus': 'public',
+        'selfDeclaredMadeForKids': False
+    },
+    'notifySubscribers': False
+}
+
+mediaFile = MediaFileUpload('test.mp4')
+
+response_upload = service.videos().insert(
+    part='snippet,status',
+    body=request_body,
+    media_body=mediaFile
+).execute()
+
+
+service.thumbnails().set(
+    videoId=response_upload.get('id'),
+    media_body=MediaFileUpload('we.png')
+).execute()
